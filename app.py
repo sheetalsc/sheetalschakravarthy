c06572e7bab976b02c8895a7baf644732f1d1c3f
Sheetal S Chakravarthy <89388394+sheetalsc@users.noreply.github.com>
2024-01-02 11:40:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Sheetal S Chakravarthy, 1BM20IS142</h2>'
+    return '<h1>Sheetal S Chakravarthy 1BM20IS142</h2>'
 
 
 if __name__ == "__main__":
